8b67d0bb71a7b745e4503cee260933bca30bea6a
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-09-20 10:56:56 +0200

    Got rid of inefficient query SHOW FIELD KEYS

--- influx-backup.py
+++ influx-backup.py
@@ -16,6 +16,9 @@ import time
 
 import requests
 
+from urllib.parse import urlparse
+from influxdb import InfluxDBClient
+
 READ_CHUCK_SIZE = 10000
 WRITE_CHUNK_SIZE = 5000
 
@@ -146,11 +149,55 @@ def dump(db, where):
     queries = []
     for m in measurements:
         if RETENTION:
-            queries.append(f'SHOW FIELD KEYS FROM "{db}"."{RETENTION}"."{m}"')
+            queries.append(f'SELECT *::field FROM "{db}"."{RETENTION}"."{m}"')
         else:
-            queries.append(f'SHOW FIELD KEYS FROM "{m}"')
+            queries.append(f'SELECT *::field FROM "{m}"')
 
-    data = query_influxdb(params={'q': ';'.join(queries), 'db': db})
+    parsed_url = urlparse(URL)
+    host = parsed_url.hostname
+    port = parsed_url.port
+    if not port:
+        port = 80
+
+    client = InfluxDBClient(host, port, AUTH[0], AUTH[1], db)
+    results = client.query(';'.join(queries))
+
+    measurement_name = ''
+    if isinstance(results, list) and len(results) > 0 and isinstance(results[0], dict):
+        keys = list(results[0].keys())
+        if keys:
+            measurement_name = keys[0]
+    else:
+        print("Invalid format for 'results'.")
+
+    field_metadata = {}
+
+    for point in results.get_points():
+      time = point['time']
+      for field, value in point.items():
+        if field not in ['time']:
+          field_metadata[field] = str(type(value)).split("'")[1]
+
+      point['fieldKey'] = list(field_metadata.keys())
+      point['fieldType'] = list(field_metadata.values())
+
+    data = {
+    'results': [
+            {
+            'statement_id': 0,
+            'series': [
+                    {
+                    'name': measurement_name,
+                    'columns': ['fieldKey', 'fieldType'],
+                    'values': [[field_key, field_type] for field_key, field_type in field_metadata.items()]
+                    }
+            ]
+            }
+    ]
+    }
+
+    client.close()
+
     msfields = {}
     for i in data['results']:
         # Empty measurement has no fields.
